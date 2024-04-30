42771f7385d8230e4eb7e4e1225748675d18bb39
Cristopher1728 <168488565+Cristopher1728@users.noreply.github.com>
2024-04-30 18:23:49 -0500

    Actualizar termux.sh

--- termux.sh
+++ termux.sh
@@ -1,4 +1,7 @@
 termux-setup-storage
 pkg update && pkg upgrade -y
 pkg install ruby wget neofetch htop
-wget https://github.com/busyloop/lolcat/archive/master.zip unzip master.zip cd lolcat-master/bin gem install lolcat
+wget https://github.com/busyloop/lolcat/archive/master.zip
+unzip master.zip
+cd lolcat-master/bin
+gem install lolcat
