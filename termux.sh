44b684536bc6d9bbf13faa8bf3fe26083dab48dd
Cristopher1728 <168488565+Cristopher1728@users.noreply.github.com>
2024-04-30 19:39:47 -0500

    Actualizar termux.sh

--- termux.sh
+++ termux.sh
@@ -5,4 +5,12 @@ wget https://github.com/busyloop/lolcat/archive/master.zip
 unzip master.zip
 cd lolcat-master/bin
 gem install lolcat
+
+echo "" >> $PREFIX/etc/bash.bashrc
+echo "" >> $PREFIX/etc/bash.bashrc
+echo "" >> $PREFIX/etc/bash.bashrc
+echo "clear" >> $PREFIX/etc/bash.bashrc
+echo "neofetch |lolcat -as 500" >> $PREFIX/etc/bash.bashrc
+echo "alias sh1='cd $PREFIX/etc && nano bash.bashrc'" >> $PREFIX/etc/bash.bashrc
+
 neofetch |lolcat -as 500
