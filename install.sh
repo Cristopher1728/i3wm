982b9eebdc84c46e8a99ee59ecee08a688675338
Cristopher1728 <168488565+Cristopher1728@users.noreply.github.com>
2024-05-01 00:07:33 -0500

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install i3 chromium chromium-l10n suckless-tools i3blocks lightdm nitrogen rofi lxappearance picom numlockx pulseaudio pulseaudio-utils pavucontrol arc-theme papirus-icon-theme blueman vokoscreen lolcat psensor polybar tilix neofetch htop btop glances xarchiver pcmanfm synaptic gparted libreoffice gimp vlc conky conky-all
 
 sudo apt install curl && sudo curl -fsSLo /usr/share/keyrings brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list && sudo apt update && sudo apt install brave-browser
+
+sudo wget https://github.com/Ulauncher/Ulauncher/releases/download/5.15.7/ulauncher_5.15.7_all.deb
+sudo dpkg -I ulauncher_5.15.7_all.deb
+sudo apt-get install -f
