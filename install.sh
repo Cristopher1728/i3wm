60bf4952f93e20fb2b67a1514d1540abba7f6c21
Cristopher1728 <168488565+Cristopher1728@users.noreply.github.com>
2024-05-02 18:54:15 -0500

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -13,35 +13,31 @@ echo "
 #Activar rofi ( rofi-theme-selector )
 bindsym $mod+d exec --no-startup-id rofi -show drun -font 'hack 13' -show combi -icon-theme 'Papirus' -show-icons
 echo "exec --no-startup-id picom" >> ~/.config/i3/config
-echo "exec --no-startup-id nitrogen --restore" >> ~/.config/i3/config
-echo "exec --no-startup-id polybar" >> ~/.config/i3/config
-echo "exec --no-startup-id ulauncher" >> ~/.config/i3/config
-echo "exec --no-startup-id conky" >> ~/.config/i3/config
+exec --no-startup-id nitrogen --restore
+exec --no-startup-id polybar
+exec --no-startup-id ulauncher
+exec --no-startup-id conky
 
-echo "" >> ~/.config/i3/config
-echo "#Ejecutar espaciado entre ventanas" >> ~/.config/i3/config
-echo "gaps outer 0" >> ~/.config/i3/config
-echo "gaps inner 7" >> ~/.config/i3/config
+#Ejecutar espaciado entre ventanas
+gaps outer 0
+gaps inner 7
 
-echo "" >> ~/.config/i3/config
-echo "#Grosor de las lineas de las ventanas" >> ~/.config/i3/config
-echo "for_window [class="^.*"] border pixel 3" >> ~/.config/i3/config
+#Grosor de las lineas de las ventanas
+for_window [class="^.*"] border pixel 3
 
-echo "" >> ~/.config/i3/config
+#class                                border            backgr.             text            indicador        child_border
 
-echo "#class                                border            backgr.             text            indicador        child_border" >> ~/.config/i3/config
+client.focused                    #4c7899        #285577        #ffffff           #3BFF00          #3BFF00
 
-echo "client.focused                    #4c7899        #285577        #ffffff           #3BFF00          #3BFF00" >> ~/.config/i3/config
+client.focused_inactive     #33333          #5f676a         #ffffff           #484e50         #FF00FF
 
-echo "client.focused_inactive     #33333          #5f676a         #ffffff           #484e50         #FF00FF" >> ~/.config/i3/config
+client.unfocused                #333333        #222222        #888888      #292d2e         #222222
 
-echo "client.unfocused                #333333        #222222        #888888      #292d2e         #222222" >> ~/.config/i3/config
+client.urgent                      #2f343a         #900000        #ffffff          #900000         #FF0000
 
-echo "client.urgent                      #2f343a         #900000        #ffffff          #900000         #FF0000" >> ~/.config/i3/config
+client.placeholder             #000000        #0c0c0c         #ffffff          #000000         #00FFFF
 
-echo "client.placeholder             #000000        #0c0c0c         #ffffff          #000000         #00FFFF" >> ~/.config/i3/config
-
-echo "client.background #ffffff" >> ~/.config/i3/config
+client.background #ffffff"
 
 echo "" >> $PREFIX/etc/bash.bashrc
 echo "" >> $PREFIX/etc/bash.bashrc
