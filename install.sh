7fa760888134f3872c43dd1e475ce5a6d6c9f411
Cristopher1728 <168488565+Cristopher1728@users.noreply.github.com>
2024-05-03 23:12:01 -0500

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install i3 chromium chromium-l10n suckless-tools i3blocks lightdm nitrogen rofi lxappearance picom numlockx pulseaudio pulseaudio-utils pavucontrol arc-theme papirus-icon-theme blueman vokoscreen lolcat psensor polybar tilix neofetch htop btop glances xarchiver pcmanfm synaptic gparted libreoffice gimp vlc conky conky-all
+sudo apt install chromium chromium-l10n suckless-tools i3blocks nitrogen rofi lxappearance picom numlockx pulseaudio pulseaudio-utils pavucontrol arc-theme papirus-icon-theme blueman vokoscreen lolcat psensor polybar tilix neofetch htop btop glances xarchiver pcmanfm synaptic gparted libreoffice gimp vlc conky conky-all
 
 sudo apt install curl && sudo curl -fsSLo /usr/share/keyrings brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list && sudo apt update && sudo apt install brave-browser
 
