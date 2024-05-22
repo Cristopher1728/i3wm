8db953b26aeb6ce97dd467d851d1a40cc67ef57c
Cristopher1728 <cristiano1728@gmail.com>
2024-05-22 00:05:21 -0500

    x

--- install.sh
+++ install.sh
@@ -21,8 +21,12 @@ sudo apt update
 sudo apt install brave-browser
 
 echo "Instalando ULAUNCHER . . ."
-sudo add-apt-repository universe -y
-sudo add-apt-repository ppa:agornostal/ulauncher -y
+sudo apt update && sudo apt install -y gnupg
+gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
+gpg --export 0xfaf1020699503176 | sudo tee /usr/share/keyrings/ulauncher-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] \
+          http://ppa.launchpad.net/agornostal/ulauncher/ubuntu jammy main" \
+          | sudo tee /etc/apt/sources.list.d/ulauncher-jammy.list
 sudo apt update && sudo apt install ulauncher
 
 echo "
