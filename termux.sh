56ab401ea476e5ebb966652cb5a38b84ae4f1788
cristopher <cristiano1738@gmail.com>
2024-05-26 14:47:48 -0500

    edit termux install

--- termux.sh
+++ termux.sh
@@ -1,30 +1,36 @@
-echo -e "\x1b[1;37m\x1b[45m* 🌎 ┼─┼┼┼─>> Inicinado Instalación de paquetes Necesarios . . ."
+#!/bin/bash
+# Creador: Cristhian Gabriel BONILLA BARRIOS <cristiano1728@gmail.com>
+# Script que instala paquetes adicionales y pre-configura i3wm en Debian y Derivados.
+#==============================================================\
+
+#Iniciando Instalación . . .
+echo -e "\e[1;37;45m* 🌎 ┼─┼┼┼─>> Inicinado Instalación de paquetes Necesarios . . . \e[1;39;49m"
 termux-setup-storage
 pkg update && pkg upgrade -y
 
-echo -e "\x1b[1;37m\x1b[45m* 🌎 ┼─┼┼┼─>> Instalación de LOLCAT . . ."
+echo -e "\e[1;37;45m* 🌎 ┼─┼┼┼─>> Instalando LOLCAT . . . \e[1;39;49m"
 pkg install ruby wget fastfetch vim htop w3m sc mc cmatrix -y
 wget https://github.com/busyloop/lolcat/archive/master.zip
 unzip master.zip
 cd lolcat-master/bin
-gem install lolcat
+gem install lolcat -y
 
-echo -e "\x1b[1;37m\x1b[45m* 🌎 ┼─┼┼┼─>> Integrando comandos en bash.bashrc . . ."
+echo -e "\e[1;37;45m* 🌎 ┼─┼┼┼─>> Integrando comandos en el archivo bash.bashrc. . . \e[1;39;49m"
 echo "
 
 
 export PS1='\e[1;39;49m\e[1;38;41m┌┼─┼─\e[1;38;42m[🕞\d\e[1;38;44m📱Cristopher📲\e[1;38;40m㉿\e[1;38;45m🌎\H]\e[1;39;49m\n\e[1;38;41m├┼─┼─\e[1;31;49m┼┼》[\@][\e[1;32;49m📁\w\e[1;31;49m]\e[1;39;49m\n\e[1;38;41m└┼─┼─\e[1;31;49m┼┼》\e[1;39;49m'
 
 clear
-fastfetch |lolcat -as 500
-alias sh1='cd $PREFIX/etc && nano bash.bashrc'
+fastfetch |lolcat -as 550
+alias sh1='cd $PREFIX/etc && nano -l -$ bash.bashrc'
 alias up='pkg upd && pkg upg -y'
-alias ne='clear && fastfetch |lolcat -as 250'
+alias ne='clear && fastfetch |lolcat -as 550'
 alias x='exit'
-
+alias nanno='nano -l -$'
 alias i='cd ~ && ls -l'" >> $PREFIX/etc/bash.bashrc
 
 clear
 cmatrix
-fastfetch |lolcat -as 500
-echo -e "\x1b[1;37m\x1b[45m* 🌎 ┼─┼┼┼─>> Instalación Finalizadq, disfrute del programa, bye . . ."
+fastfetch |lolcat -as 550
+echo -e "\e[1;37;45m* 🌎 ┼─┼┼┼─>> Instalación Finalizada, Disfrute del Sistema y Aplicaciones Instalados . . . \e[1;39;49m"
