1a9d5db6bf3c9ea48ae85f680cec62a9cb8dd9cd
cristopher <cristiano1728@gmail.com>
2024-06-09 10:32:53 -0500

    excluir paquetes install

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt update && upgrade -y
 sudo apt install curl nitrogen rofi lxappearance picom numlockx pulseaudio pulseaudio-utils pavucontrol arc-theme papirus-icon-theme lolcat cmatrix psensor polybar neofetch htop btop glances xarchiver pcmanfm synaptic gparted conky conky-all gthumb blueman lxpolkit telegram-desktop connman connman-gtk -y
 echo -e "\e[1;37;45m* 🌎 ┼─┼┼┼─>> Paquetes Adicionales para un Sistema más Completo . . . \e[1;39;49m"
 #Paquetes Más usados
-sudo apt install chromium chromium-l10n libreoffice libreoffice-l10n-es gimp vlc notepadqq kate okular simplescreenrecorder -y
+#sudo apt install chromium chromium-l10n libreoffice libreoffice-l10n-es gimp vlc notepadqq kate okular simplescreenrecorder -y
 
 nitrogen
 
