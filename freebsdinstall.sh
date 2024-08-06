168f8e03b355c792e3e11846e994c66866469758
cristopher1728 <cristiano1728@gmail.com>
2024-08-06 02:05:44 -0500

    Edit lightdm en ves q slim

--- freebsdinstall.sh
+++ freebsdinstall.sh
@@ -7,17 +7,13 @@
 echo -e "\e[1;37;45m* ㉿ ┼─┼┼┼─>> Inicinado Instalación de paquetes Necesarios . . . \e[1;39;49m"
 #Paquetes basicos para el sistema
 sudo pkg update && sudo pkg upgrade
-sudo pkg install sudo xorg i3 tilix i3status i3blocks i3lock slim curl nitrogen rofi lxappearance picom numlockx pulseaudio pavucontrol papirus-icon-theme gtk-arc-themes lolcat cmatrix polybar neofetch htop btop xarchiver pcmanfm conky gthumb telegram-desktop lightdm-gtk-greeter i3status i3blocks sc-im
-
-echo -e "\e[1;37;45m* ㉿ ┼─┼┼┼─>> Paquetes Adicionales para un Sistema más Completo . . . \e[1;39;49m"
-#Paquetes Más usados
-sudo pkg install chromium es-libreoffice gimp vlc kate okular simplescreenrecorder ulauncher
+sudo pkg install sudo xorg i3 tilix i3status i3blocks i3lock lightdm nano curl nitrogen rofi lxappearance picom numlockx pulseaudio pavucontrol papirus-icon-theme gtk-arc-themes lolcat cmatrix polybar neofetch htop btop xarchiver pcmanfm conky gthumb telegram-desktop lightdm-gtk-greeter i3status i3blocks sc-im chromium es-libreoffice gimp vlc kate okular simplescreenrecorder ulauncher
 
 sudo sysrc hald_enable="YES"
 sudo sysrc dbus_enable="YES"
-sudo sysrc slim_enable="YES"
+sudo sysrc lightdm_enable="YES"
 
-echo "/usr/local/bin/i3" >> ~/.xinitrc
+echo "exec i3" >> ~/.xinitrc
 
 #Mover archivos de configuración a la carpeta de Nitrogen - Fondo de Pantalla
 echo -e "\e[1;37;45m* ㉿ ┼─┼┼┼─>> Insertando Archivos a la carpeta de Configuración de ROFI y LIGHTDM . . . \e[1;39;49m"
@@ -28,6 +24,8 @@ sudo mv -f themes /usr/share/rofi
 mkdir ~/.config/rofi
 mv rofi ~/.config
 sudo mv -f i3status.conf i3blocks.conf /etc
+sudo mkdir /etc/lightdm
+sudo mv -f lightdm-gtk-greeter.conf /etc/lightdm
 
 #Mover archivos de configuración a la carpeta de Nitrogen - Pantalla
 echo -e "\e[1;37;45m* ㉿ ┼─┼┼┼─>> Insertando Archivos a la carpeta de Config de NITROGEN . . . \e[1;39;49m"
@@ -38,7 +36,7 @@ sudo mv -f bg-saved.cfg nitrogen.cfg ~/.config/nitrogen
 mkdir ~/.config
 mkdir ~/.config/i3
 mv config ~/.config/i3/
-#mv -f .shrc ~
+mv -f .shrc ~
 
 cmatrix
 neofetch |lolcat
